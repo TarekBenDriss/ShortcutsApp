fdb809158da54b3f30f0ca98095806f9eaf37e12
Tarek <tarek.bendriss@natan.fr>
2019-12-03 16:16:51 +0100

    Main activity

--- app/src/main/java/com/bendriss/shortcutsapp/MainActivity.java
+++ app/src/main/java/com/bendriss/shortcutsapp/MainActivity.java
@@ -1,14 +1,39 @@
 package com.bendriss.shortcutsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    ConstraintLayout callLayout,messagesLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        callLayout = findViewById(R.id.callLayout);
+        messagesLayout = findViewById(R.id.messagesLayout);
+
+        callLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,CallActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        messagesLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MessagesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/bendriss/shortcutsapp/MessagesActivity.java
@@ -0,0 +1,18 @@
+package com.bendriss.shortcutsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MessagesActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messages);
+    }
+}
